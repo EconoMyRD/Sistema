20a1a74f32d136f28fca491fbb6ad1ac186073cd
ricardo jonas faria <ricardo.jonas.faria@gmail.com>
2015-06-12 18:15:51 -0300

    removed wrong folders

--- Economy/javascript/import.js
+++
@@ -1,8 +0,0 @@
-
-var link = document.querySelector('link[rel="import"]');
-var content = link.import;
-
-var el;
-el = content.querySelector('#commonContent');
-
-document.body.appendChild(el.cloneNode(true));
--- Economy/javascript/lancamento.js
+++
@@ -1,5 +0,0 @@
-var doc= this.document;
-
-doc.getElementsByName("submit").onclick = function (e){
-   alert("alerta"); alert(document.getElementsByName("descricao").value);    
-};
