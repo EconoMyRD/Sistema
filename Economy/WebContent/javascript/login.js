5d7602ef17b9cdde47e06f83f36a55ce0a78cfca
DanielaMara <danielamaradms@gmail.com>
2015-06-17 21:33:23 -0300

    Atualização do HTML e inserção de novas imagens

--- Economy/WebContent/javascript/login.js
+++ Economy/WebContent/javascript/login.js
@@ -22,6 +22,7 @@ $(document).ready(function()
     $('.menu-interno').click(function() 
     {
         $("#conteudo").load($(this).attr('href'));
+        $("#conteudoInicial").hide();
         return false;
     });    
 });
